de1742ef6f7e6964bdd3c9a6eab2cbc0e7bf3ef7
yohanaaeva <73064568+yohanaaeva@users.noreply.github.com>
2021-01-12 18:08:50 +0700

    Add files via upload

---
+++ Project_Komunikasi_Wireless_Arduino_XBEE_Penerima.ino
@@ -0,0 +1,46 @@
+int Motor1 = 7;
+int Motor2 = 6;
+int Motor3 = 5;
+int Motor4 = 4;
+int DataCheck = 0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(Motor1, OUTPUT);
+  pinMode(Motor2, OUTPUT);
+  pinMode(Motor3, OUTPUT);
+  pinMode(Motor4, OUTPUT);
+
+  digitalWrite(Motor1, HIGH);
+  digitalWrite(Motor2, HIGH);
+  digitalWrite(Motor3, HIGH);
+  digitalWrite(Motor4, HIGH);
+  Serial.print("Simulasi Pengendali Motor DC dengan XBEE:");
+  Serial.println();
+  Serial.println("TELEKOMUNIKASI 3B");
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  
+}
+void loop() {
+{
+  if(Serial.available())
+  {
+    char data = Serial.read();
+    Serial.print(data);
+    Serial.print("      ========>      ");
+
+    if(data == '1'){DataCheck = 1; digitalWrite(Motor2, LOW);digitalWrite(Motor1, HIGH); Serial.println("Motor pertama berputar searah jarum jam.");}
+    if(data == '2'){DataCheck = 1; digitalWrite(Motor1, LOW);digitalWrite(Motor2, HIGH); Serial.println("Motor pertama berputar berlawanan arah jarum jam.");}
+    if(data == '3'){DataCheck = 1; digitalWrite(Motor1, LOW);digitalWrite(Motor2, LOW); Serial.println("Motor pertama BERHENTI");}
+    if(data == '4'){DataCheck = 1; digitalWrite(Motor3, LOW);digitalWrite(Motor4, HIGH); Serial.println("Motor kedua berputar searah jarum jam.");}
+    if(data == '5'){DataCheck = 1; digitalWrite(Motor4, LOW);digitalWrite(Motor3, HIGH); Serial.println("Motor kedua berputar berlawanan arah jarum jam.");}
+    if(data == '6'){DataCheck = 1; digitalWrite(Motor3, LOW);digitalWrite(Motor4, LOW); Serial.println("Motor kedua BERHENTI");}
+    if(DataCheck == 0){Serial.println("Perintah salah, silahkan ulangi !!! ");}
+    Serial.println();
+    DataCheck = 0;
+    }
+  }
+}
